cccb6b18569d65c823f124be16f4e04e9328899a
David Trannam <trannamd@oregonstate.edu>
2023-03-11 23:24:23 -0800

    added styling, sorting, error handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import states from "./data/stateAbb.json"
 import jobs from "./data/salary.json"
 import InputLabel from "@mui/material/InputLabel";
 import Table from '@mui/material/Table';
@@ -11,13 +10,29 @@ import TableHead from '@mui/material/TableHead';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import TableRow from '@mui/material/TableRow';
 import MenuItem from '@mui/material/MenuItem';
+import { red } from "@mui/material/colors";
+import { fontSize, style } from "@mui/system";
 
 function App() {
 
   const [search, setSearch] = useState("")
   const [filterBy, setFilterBy] = useState({value: 'name'})
-  const [filterState, setfilterState] = useState({value: `CA`})
   const [jobFilter, setJobFilters] = useState(jobs)
+  const [costOfLiving, setCostOfLiving] = useState(0)
+  const [visible, setVisible] = useState("hidden")
+
+  // Calls Microservice to get data 
+  const serviceURL = "http://localhost:4000/laCol"
+  async function getCostOfLiving() {
+    let jsonResponse = await fetch(serviceURL).then(response => response.json()).catch(console.log("Data Failed to Load"))
+    let monthTotal = jsonResponse[1]["Cost of Living Month Total"]
+    let oneBedroom = jsonResponse[1]["Rent Per Month prices"][0]["Value"]
+    setCostOfLiving(parseInt(monthTotal.replace(/,/g, '')) + parseInt(oneBedroom.replace(/,/g, '')))
+  }
+  useEffect(() => {
+    getCostOfLiving()
+    }
+  , [])
 
   function onChangeSearch(event) {
     setSearch(event.target.value)
@@ -27,26 +42,27 @@ function App() {
     setFilterBy({value: event.target.value})
   }
 
-  function onChangeFilterState(event) {
-    setfilterState({value: event.target.value})
-  }
-
 
   useEffect(() => {
     let tempData = jobs
     if (filterBy.value === 'name') {
-      tempData = tempData.filter(job => job.Title.toLowerCase().includes(search.toLowerCase()))
+      tempData = tempData.filter(job => job.Title.toLowerCase().includes(search.toLowerCase())).sort((a,b ) => a.Title - b.Title)
     } else if (filterBy.value === 'income' && isNaN(search)) {
       // Value is searching for an income, but the search field is not a number
-      alert("Enter a number if searching by Income")
+      setSearch("")
+      setVisible("visible")
+      setTimeout(() => {
+        setVisible("hidden")}, 2500
+      )
     } else {
       // job.Annual.replace(",", "") - 5000 <= search || search >= job.Annual.replace(",", "") + 5000
-      tempData = tempData.filter(job => parseInt(job.Annual.replace(",", "")) >= parseInt(search))
+      tempData = tempData.filter(job => parseInt(job.Annual.replace(",", "")) >= parseInt(search)).sort((a, b) => parseInt(a.Annual.replace(",", "")) - parseInt(b.Annual.replace(",", "")))
     }
     setJobFilters(tempData)
   
-  }, [search, filterBy, filterState])
+  }, [search, filterBy])
 
+  // Base URL to present to user
   var baseURL = "https://www.bls.gov/oes/current/oes_nat.htm"
 
   return (
@@ -60,22 +76,24 @@ function App() {
               <MenuItem value="income">Income</MenuItem>
           </Select>
 
-          <TextField label="Search Term*" onChange={onChangeSearch}/>
-
-          <InputLabel>Select A State</InputLabel>
-          <Select onChange={onChangeFilterState} value={filterState.value}>
-              {states.map(state => 
-                    <MenuItem value={state.Abbreviations} key={state.Abbreviations}>{state.Name}</MenuItem>
-                )}
-          </Select>
-
+          <TextField label="Search Term*" value={search} onChange={onChangeSearch}/>
+          <p style={{visibility: `${visible}`}}>The value entered was not valid</p>
         </form>
-          Data used from this website is pulled directly from the US Bureau of Labor Statistics May 2021 Report (https://www.bls.gov/oes/current/oes_nat.htm). This project expands on the BLS gov site by allowing better searching through income or a job title search.
-          Note: This data is from the May 2021 Report and may not be up to date
-        <TableContainer>
+          <br/>
+          <h3>Usage</h3>
+          <p>This project expands on the BLS gov site by allowing better searching through income or a job title search. Income search will yield any salary that exceed the values</p>
+          <h3>About</h3>
+          <p>
+          Data used from this website is pulled directly from the <b>US Bureau of Labor Statistics May 2021 Report ({baseURL})</b>. 
+          </p>
+          <p>
+          Percentage of income is a reflection of the annual cost of living in Los Angeles with rent for a one bedroom apartment in city centre.
+          </p>
+          <br/>
+        <TableContainer className="Table">
           <Table>
-            <TableHead>
-                <TableCell>Job Title</TableCell>
+            <TableHead className="Header">
+                <TableCell >Job Title</TableCell>
                 <TableCell>Annual Mean Wage</TableCell>
                 <TableCell>Percentage of Income</TableCell>
                 <TableCell>Median Hourly Wage</TableCell>
@@ -85,12 +103,12 @@ function App() {
             <TableBody>
                 {
                   jobFilter.map(row => {
-                    return <TableRow key={row.Code}>
+                    return <TableRow key={row.Code} className="Row">
                       <TableCell onClick={() => alert("You are navigating away from this site and onto the BLS gov site.")}>
                           <a href={`https://www.bls.gov/oes/current/oes${row.Code}.htm`}> {row.Title} </a>
                       </TableCell>
                       <TableCell>${row.Annual}</TableCell>
-                      <TableCell>N/A</TableCell>
+                      <TableCell>{costOfLiving ? (costOfLiving*12/parseInt(row.Annual.replace(/,/g, ''))).toFixed(2) + "%" : "N/A"}</TableCell>
                       <TableCell>${row.Median}</TableCell>
                       <TableCell>${row.Mean}</TableCell>
                       <TableCell>${row.EmploymentPer1000}</TableCell>
